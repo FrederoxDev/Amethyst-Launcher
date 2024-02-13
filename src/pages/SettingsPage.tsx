50d6ab3a8a1c589c69b8a48608ef81f6b4055c0a
FrederoxDev <blakefreddie96@gmail.com>
2024-02-13 18:22:15 +0000

    Open mods folder button

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -1,9 +1,29 @@
+import DividedSection from "../components/DividedSection";
 import MainPanel from "../components/MainPanel";
+import MinecraftButton from "../components/MinecraftButton";
 import ToggleSection from "../components/ToggleSection";
 import { useAppState } from "../contexts/AppState";
+import { getMinecraftFolder } from "../versionSwitcher/VersionManager";
+const fs = window.require('fs') as typeof import('fs');
+const child = window.require('child_process') as typeof import('child_process')
 
 export default function SettingsPage() {
     const { keepLauncherOpen, setKeepLauncherOpen, developerMode, setDeveloperMode } = useAppState()
+
+    const openModsFolder = () => {
+        // Don't reveal in explorer unless there is an existing minecraft folder
+        if (!fs.existsSync(getMinecraftFolder())) {
+            alert("Minecraft is not currently installed");
+            return;
+        }
+
+        const folder = getMinecraftFolder() + "\\AC\\Amethyst\\mods\\";
+
+        if (!fs.existsSync(folder)) fs.mkdirSync(folder, { recursive: true });
+
+        const startGameCmd = `explorer "${folder}"`;
+        child.spawn(startGameCmd, { shell: true })
+    }
 
     return (
         <MainPanel>
@@ -19,6 +39,12 @@ export default function SettingsPage() {
                 isChecked={developerMode}
                 setIsChecked={setDeveloperMode}
             />
+            <DividedSection className="flex-grow flex justify-around gap-[8px]">
+                <div className="h-full flex flex-col"></div>
+            </DividedSection>
+            <DividedSection>
+                <MinecraftButton text="Open Mods Folder" onClick={openModsFolder} />
+            </DividedSection>
         </MainPanel>
     )
 }
