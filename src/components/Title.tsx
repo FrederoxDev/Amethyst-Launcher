d0703df63a5f72fcb070436553cb2ab0c377142f
Adrian <adrian.olbrich1@gmail.com>
2024-06-19 20:59:10 +0200

    get app version in title

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,11 +1,21 @@
+import {useEffect, useState} from "react";
+const { ipcRenderer } = window.require('electron');
+
 export default function Header() {
+    const [appVersion, setAppVersion] = useState('-');
+
+    useEffect(() => {
+        ipcRenderer.invoke('get-app-version').then((version) => {
+            setAppVersion(version);
+        });
+    }, []);
+
     return (
         <>
             <div className="h-[42px] bg-[#E6E8EB] flex flex-col justify-center items-center">
                 <p className="minecraft-ten block translate-y-[6px]">Amethyst Launcher</p>
-                <p className="minecraft-seven block text-[12px] text-[#464749]">v2.3.0</p>
+                <p className="minecraft-seven block text-[12px] text-[#464749]">{appVersion}</p>
             </div>
-
 
             {/* Shadow/Highlight */}
             <div className="bg-[#EBEDEF] h-[2px]"></div>
