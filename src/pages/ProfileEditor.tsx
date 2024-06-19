3130c47bfc5ecb0c24cf86728cbccf70972107a3
Adrian <adrian.olbrich1@gmail.com>
2024-06-19 22:34:26 +0200

    reformat

--- src/pages/ProfileEditor.tsx
+++ src/pages/ProfileEditor.tsx
@@ -95,61 +95,61 @@ export default function ProfileEditor() {
     }, [setAllMods]);
 
     return (
-      <MainPanel>
-          {/* Settings */}
-          <DividedSection>
-              <TextInput label="Profile Name" text={profileName} setText={setProfileName} />
-              <Dropdown
-                labelText="Minecraft Version"
-                value={ profileMinecraftVersion }
-                setValue={ setProfileMinecraftVersion }
+        <MainPanel>
+            {/* Settings */}
+            <DividedSection>
+                <TextInput label="Profile Name" text={profileName} setText={setProfileName} />
+                <Dropdown
+                  labelText="Minecraft Version"
+                  value={ profileMinecraftVersion }
+                  setValue={ setProfileMinecraftVersion }
 
-                // we don't support non-release versions right now so only show release lmao
-                options={ allMinecraftVersions.filter(ver => ver.versionType == VersionType.Release).map(ver => ver.toString()) }
-                id="minecraft-version"
-              />
-              <Dropdown
-                labelText="Runtime"
-                value={ profileRuntime }
-                setValue={ setProfileRuntime }
-                options={ allRuntimes }
-                id="runtime-mod"
-              />
-          </DividedSection>
+                  // we don't support non-release versions right now so only show release lmao
+                  options={ allMinecraftVersions.filter(ver => ver.versionType == VersionType.Release).map(ver => ver.toString()) }
+                  id="minecraft-version"
+                />
+                <Dropdown
+                  labelText="Runtime"
+                  value={ profileRuntime }
+                  setValue={ setProfileRuntime }
+                  options={ allRuntimes }
+                  id="runtime-mod"
+                />
+            </DividedSection>
 
-          {/* Mod Selection */}
-          {
-              profileRuntime === "Vanilla"
-                ? <DividedSection className="flex-grow flex justify-around gap-[8px]">
-                    <div className="h-full flex flex-col"></div>
-                </DividedSection>
-                : <DividedSection className="flex-grow flex justify-around gap-[8px]">
-                    <div className=" w-[50%] h-full flex flex-col">
-                        <p className="text-white minecraft-seven">Active Mods</p>
-                        <div className="border-[2px] border-[#1E1E1F] bg-[#313233] flex-grow">
-                            {
-                                allMods.length > 0 ? allMods.filter(mod => profileActiveMods.includes(mod))
-                                  .map((mod, index) => <ModButton name={mod} key={index} />) : <></>
-                            }
-                        </div>
-                    </div>
-                    <div className=" w-[50%] h-full flex flex-col">
-                        <p className="text-white minecraft-seven">Inactive Mods</p>
-                        <div className="border-[2px] border-[#1E1E1F] bg-[#313233] flex-grow">
-                            {
-                                allMods.length > 0 ? allMods.filter(mod => !profileActiveMods.includes(mod))
-                                  .map((mod, index) => <ModButton name={mod} key={index} />) : <></>
-                            }
-                        </div>
-                    </div>
-                </DividedSection>
-          }
+            {/* Mod Selection */}
+            {
+                profileRuntime === "Vanilla"
+                  ? <DividedSection className="flex-grow flex justify-around gap-[8px]">
+                      <div className="h-full flex flex-col"></div>
+                  </DividedSection>
+                  : <DividedSection className="flex-grow flex justify-around gap-[8px]">
+                      <div className=" w-[50%] h-full flex flex-col">
+                          <p className="text-white minecraft-seven">Active Mods</p>
+                          <div className="border-[2px] border-[#1E1E1F] bg-[#313233] flex-grow">
+                              {
+                                  allMods.length > 0 ? allMods.filter(mod => profileActiveMods.includes(mod))
+                                    .map((mod, index) => <ModButton name={mod} key={index} />) : <></>
+                              }
+                          </div>
+                      </div>
+                      <div className=" w-[50%] h-full flex flex-col">
+                          <p className="text-white minecraft-seven">Inactive Mods</p>
+                          <div className="border-[2px] border-[#1E1E1F] bg-[#313233] flex-grow">
+                              {
+                                  allMods.length > 0 ? allMods.filter(mod => !profileActiveMods.includes(mod))
+                                    .map((mod, index) => <ModButton name={mod} key={index} />) : <></>
+                              }
+                          </div>
+                      </div>
+                  </DividedSection>
+            }
 
-          {/* Profile Actions */}
-          <DividedSection className="flex justify-around gap-[8px]">
-              <div className="w-[50%]"><MinecraftButton text="Save Profile" onClick={() => saveProfile()} /></div>
-              <div className="w-[50%]"><MinecraftButton text="Delete Profile" style={MinecraftButtonStyle.Warn} onClick={() => deleteProfile()}/></div>
-          </DividedSection>
-      </MainPanel>
+            {/* Profile Actions */}
+            <DividedSection className="flex justify-around gap-[8px]">
+                <div className="w-[50%]"><MinecraftButton text="Save Profile" onClick={() => saveProfile()} /></div>
+                <div className="w-[50%]"><MinecraftButton text="Delete Profile" style={MinecraftButtonStyle.Warn} onClick={() => deleteProfile()}/></div>
+            </DividedSection>
+        </MainPanel>
     )
 }
