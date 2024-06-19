fc95d8a5221150dd7cde95e418bce342dd66bf0c
Adrian <adrian.olbrich1@gmail.com>
2024-06-19 20:59:29 +0200

    add loading wheel

---
+++ src/components/LoadingWheel.tsx
@@ -0,0 +1,17 @@
+type LoadingWheelProps = {
+    text: string,
+    percentage: number
+}
+
+export default function LoadingWheel({ text, percentage }: LoadingWheelProps) {
+    return (
+        <>
+            <div className="fixed top-0 left-0 flex flex-col w-full items-center justify-center h-full">
+                <div className="flex flex-col items-center">
+                    <p className="minecraft-seven text-white text-[14px]">{text}</p>
+                    <p className="minecraft-seven text-white text-[64px]">{percentage.toPrecision(3)}%</p>
+                </div>
+            </div>
+        </>
+    )
+}
