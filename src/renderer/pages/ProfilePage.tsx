9a249e80660b3f0764f3f078919251505754b3f7
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-07-08 23:18:02 +1200

    UI stuff

--- src/renderer/pages/ProfilePage.tsx
+++ src/renderer/pages/ProfilePage.tsx
@@ -15,7 +15,7 @@ const ProfileButton = ({profile, index}: { profile: Profile, index: number }) =>
 
     return (
         <div onClick={() => openProfile(profile, index)}>
-            <div className="cursor-pointer border-[3px] border-[#48494A] bg-[#58585A] p-[8px]">
+            <div className="cursor-pointer border-y-[3px] border-t-[#5a5b5c] border-b-[#333334] bg-[#48494a] p-[8px]">
                 <p className="minecraft-seven text-white text-[14px] px-[4px]">{profile.name}</p>
                 <p className="minecraft-seven text-[#B1B2B5] text-[14px] px-[4px]">{profile.minecraft_version} ({profile.runtime})</p>
             </div>
@@ -31,7 +31,7 @@ export default function ProfilePage() {
         <MainPanel>
             <div className="flex flex-col gap-[8px] h-full p-[8px] bg-[#48494A] border-[#1E1E1F] border-[3px] overflow-hidden">
                 <p className="minecraft-seven text-white text-[14px]">Profile Editor</p>
-                <div className="flex flex-col gap-[3px] border-[3px] border-[#1E1E1F] h-full p-[3px] bg-[#313233] overflow-y-auto scrollbar">
+                <div className="flex flex-col border-[3px] border-[#1E1E1F] h-full bg-[#313233] overflow-y-auto scrollbar">
                     
                     {
                         allProfiles.map((profile, index) => {
