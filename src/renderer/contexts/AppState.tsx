8458dff9c66b9fcf9aa67eaf4f63388f3e644102
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-07-13 00:33:35 +1200

    Revert "Merge branch 'main' of https://github.com/FrederoxDev/Amethyst-Launcher"
    This reverts commit 94a03ac559fc346e89c5e1a8dc8d56eb1cae002f, reversing
    changes made to 965b34aefd53a9e4aa8b5f443289bf9a24af5b92.

--- src/renderer/contexts/AppState.tsx
+++ src/renderer/contexts/AppState.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:src/renderer/contexts/AppState.tsx
 import {createContext, ReactNode, useCallback, useContext, useEffect, useState} from "react";
 
 import {FetchMinecraftVersions, MinecraftVersion} from "../scripts/Versions"
@@ -7,95 +6,64 @@ import {GetProfiles, Profile, SetProfiles} from "../scripts/Profiles";
 
 import { ipcRenderer } from "electron";
 import {GetMods} from "../scripts/Mods";
-=======
-import {
-	createContext,
-	ReactNode,
-	useCallback,
-	useContext,
-	useEffect,
-	useState,
-} from 'react'
-import { Profile } from '../types/Profile'
-import {
-	findAllMods,
-	findAllProfiles,
-	getAllMinecraftVersions,
-	readLauncherConfig,
-	saveAllProfiles,
-	saveLauncherConfig,
-} from '../launcher/Modlist'
-import { LauncherConfig } from '../types/LauncherConfig'
-import { MinecraftVersion } from '../types/MinecraftVersion'
-import { ipcRenderer } from 'electron'
->>>>>>> 035b376ad437fffcbc68bc32b9ec642c1a1c2c97:src/contexts/AppState.tsx
 
 interface TAppStateContext {
-	allMods: string[]
-	setAllMods: React.Dispatch<React.SetStateAction<string[]>>
+    allMods: string[];
+    setAllMods: React.Dispatch<React.SetStateAction<string[]>>;
 
-	allRuntimes: string[]
-	setAllRuntimes: React.Dispatch<React.SetStateAction<string[]>>
+    allRuntimes: string[];
+    setAllRuntimes: React.Dispatch<React.SetStateAction<string[]>>;
 
-	allMinecraftVersions: MinecraftVersion[]
-	setAllMinecraftVersions: React.Dispatch<
-		React.SetStateAction<MinecraftVersion[]>
-	>
+    allMinecraftVersions: MinecraftVersion[];
+    setAllMinecraftVersions: React.Dispatch<React.SetStateAction<MinecraftVersion[]>>;
 
-	allProfiles: Profile[]
-	setAllProfiles: React.Dispatch<React.SetStateAction<Profile[]>>
+    allProfiles: Profile[];
+    setAllProfiles: React.Dispatch<React.SetStateAction<Profile[]>>;
 
-	selectedProfile: number
-	setSelectedProfile: React.Dispatch<React.SetStateAction<number>>
+    selectedProfile: number;
+    setSelectedProfile: React.Dispatch<React.SetStateAction<number>>;
 
-	UITheme: string
-	setUITheme: React.Dispatch<React.SetStateAction<string>>
+    UITheme: string;
+    setUITheme: React.Dispatch<React.SetStateAction<string>>;
 
-	keepLauncherOpen: boolean
-	setKeepLauncherOpen: React.Dispatch<React.SetStateAction<boolean>>
+    keepLauncherOpen: boolean;
+    setKeepLauncherOpen: React.Dispatch<React.SetStateAction<boolean>>;
 
-	developerMode: boolean
-	setDeveloperMode: React.Dispatch<React.SetStateAction<boolean>>
+    developerMode: boolean;
+    setDeveloperMode: React.Dispatch<React.SetStateAction<boolean>>;
 
-	loadingPercent: number
-	setLoadingPercent: React.Dispatch<React.SetStateAction<number>>
+    loadingPercent: number;
+    setLoadingPercent: React.Dispatch<React.SetStateAction<number>>;
 
-	isLoading: boolean
-	setIsLoading: React.Dispatch<React.SetStateAction<boolean>>
+    isLoading: boolean;
+    setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
 
-	status: string
-	setStatus: React.Dispatch<React.SetStateAction<string>>
+    status: string;
+    setStatus: React.Dispatch<React.SetStateAction<string>>;
 
-	error: string
-	setError: React.Dispatch<React.SetStateAction<string>>
+    error: string;
+    setError: React.Dispatch<React.SetStateAction<string>>;
 
-	errorType: 'launch' | 'import'
-	setErrorType: React.Dispatch<React.SetStateAction<'launch' | 'import'>>
-
-	// Expose functions
-	saveData: () => void
+    // Expose functions
+    saveData: () => void
 }
 
-const AppStateContext = createContext<TAppStateContext | undefined>(undefined)
+const AppStateContext = createContext<TAppStateContext | undefined>(undefined);
 
-export const AppStateProvider = ({ children }: { children: ReactNode }) => {
-	const [allMods, setAllMods] = useState<string[]>([])
-	const [allRuntimes, setAllRuntimes] = useState<string[]>([])
-	const [allMinecraftVersions, setAllMinecraftVersions] = useState<
-		MinecraftVersion[]
-	>([])
-	const [allProfiles, setAllProfiles] = useState<Profile[]>([])
-	const [selectedProfile, setSelectedProfile] = useState(0)
-	const [UITheme, setUITheme] = useState('System')
-	const [keepLauncherOpen, setKeepLauncherOpen] = useState(true)
-	const [developerMode, setDeveloperMode] = useState(false)
-	const [loadingPercent, setLoadingPercent] = useState(0)
-	const [isLoading, setIsLoading] = useState(false)
-	const [status, setStatus] = useState('')
-	const [error, setError] = useState('')
-	const [errorType, setErrorType] = useState<'launch' | 'import'>('launch')
+export const AppStateProvider = ({children}: { children: ReactNode }) => {
+    const [allMods, setAllMods] = useState<string[]>([]);
+    const [allRuntimes, setAllRuntimes] = useState<string[]>([]);
+    const [allMinecraftVersions, setAllMinecraftVersions] = useState<MinecraftVersion[]>([]);
+    const [allProfiles, setAllProfiles] = useState<Profile[]>([]);
+    const [selectedProfile, setSelectedProfile] = useState(0);
+    const [UITheme, setUITheme] = useState("System");
+    const [keepLauncherOpen, setKeepLauncherOpen] = useState(true);
+    const [developerMode, setDeveloperMode] = useState(false);
+    const [loadingPercent, setLoadingPercent] = useState(0);
+    const [isLoading, setIsLoading] = useState(false);
+    const [status, setStatus] = useState("");
+    const [error, setError] = useState("");
 
-<<<<<<< HEAD:src/renderer/contexts/AppState.tsx
     // Initialize Data like all mods and existing profiles..
     useEffect(() => {
         setAllProfiles(GetProfiles());
@@ -114,117 +82,65 @@ export const AppStateProvider = ({ children }: { children: ReactNode }) => {
             const versions = await FetchMinecraftVersions();
             setAllMinecraftVersions(versions);
         }
-=======
-	// Initialize Data like all mods and existing profiles..
-	useEffect(() => {
-		setAllProfiles(findAllProfiles())
 
-		const modList = findAllMods()
-		setAllRuntimes(['Vanilla', ...modList.runtimeMods])
-		setAllMods(modList.mods)
+        fetchMinecraftVersions();
+    }, [])
 
-		const readConfig = readLauncherConfig()
-		setKeepLauncherOpen(readConfig.keep_open ?? true)
-		setDeveloperMode(readConfig.developer_mode ?? false)
-		setSelectedProfile(readConfig.selected_profile ?? 0)
-		setUITheme(readConfig.ui_theme ?? 'Light')
+    const [hasInitialized, setHasInitialized] = useState(false);
 
-		const fetchMinecraftVersions = async () => {
-			const versions = await getAllMinecraftVersions()
-			setAllMinecraftVersions(versions)
-		}
->>>>>>> 035b376ad437fffcbc68bc32b9ec642c1a1c2c97:src/contexts/AppState.tsx
-
-		fetchMinecraftVersions()
-	}, [])
-
-	const [hasInitialized, setHasInitialized] = useState(false)
-
-<<<<<<< HEAD:src/renderer/contexts/AppState.tsx
     const saveData = useCallback(() => {
         SetProfiles(allProfiles);
-=======
-	const saveData = useCallback(() => {
-		saveAllProfiles(allProfiles)
->>>>>>> 035b376ad437fffcbc68bc32b9ec642c1a1c2c97:src/contexts/AppState.tsx
 
-		const launcherConfig: LauncherConfig = {
-			developer_mode: developerMode,
-			keep_open: keepLauncherOpen,
-			mods: allProfiles[selectedProfile]?.mods ?? [],
-			runtime: allProfiles[selectedProfile]?.runtime ?? '',
-			selected_profile: selectedProfile,
-			ui_theme: UITheme,
-		}
+        const launcherConfig: LauncherConfig = {
+            developer_mode: developerMode,
+            keep_open: keepLauncherOpen,
+            mods: allProfiles[selectedProfile]?.mods ?? [],
+            runtime: allProfiles[selectedProfile]?.runtime ?? "",
+            selected_profile: selectedProfile,
+            ui_theme: UITheme
+        };
 
-<<<<<<< HEAD:src/renderer/contexts/AppState.tsx
         SetLauncherConfig(launcherConfig);
     }, [allProfiles, developerMode, keepLauncherOpen, selectedProfile, UITheme])
-=======
-		saveLauncherConfig(launcherConfig)
-	}, [allProfiles, developerMode, keepLauncherOpen, selectedProfile, UITheme])
->>>>>>> 035b376ad437fffcbc68bc32b9ec642c1a1c2c97:src/contexts/AppState.tsx
 
-	useEffect(() => {
-		if (!hasInitialized) {
-			setHasInitialized(true)
-			return
-		}
+    useEffect(() => {
+        if (!hasInitialized) {
+            setHasInitialized(true);
+            return;
+        }
 
-		saveData()
-	}, [
-		allProfiles,
-		selectedProfile,
-		keepLauncherOpen,
-		developerMode,
-		hasInitialized,
-		saveData,
-	])
+        saveData();
+    }, [allProfiles, selectedProfile, keepLauncherOpen, developerMode, hasInitialized, saveData])
 
-	useEffect(() => {
-		ipcRenderer.send('WINDOW_UI_THEME', UITheme)
-	}, [UITheme])
+    useEffect(() => {
+        ipcRenderer.send('WINDOW_UI_THEME', UITheme)
+    }, [UITheme])
 
-	return (
-		<AppStateContext.Provider
-			value={{
-				allMods,
-				setAllMods,
-				allRuntimes,
-				setAllRuntimes,
-				allMinecraftVersions,
-				setAllMinecraftVersions,
-				allProfiles,
-				setAllProfiles,
-				selectedProfile,
-				setSelectedProfile,
-				UITheme,
-				setUITheme,
-				keepLauncherOpen,
-				setKeepLauncherOpen,
-				developerMode,
-				setDeveloperMode,
-				loadingPercent,
-				setLoadingPercent,
-				isLoading,
-				setIsLoading,
-				status,
-				setStatus,
-				saveData,
-				error,
-				setError,
-				errorType,
-				setErrorType,
-			}}
-		>
-			{children}
-		</AppStateContext.Provider>
-	)
+    return (
+        <AppStateContext.Provider value={
+            {
+                allMods, setAllMods,
+                allRuntimes, setAllRuntimes,
+                allMinecraftVersions, setAllMinecraftVersions,
+                allProfiles, setAllProfiles,
+                selectedProfile, setSelectedProfile,
+                UITheme, setUITheme,
+                keepLauncherOpen, setKeepLauncherOpen,
+                developerMode, setDeveloperMode,
+                loadingPercent, setLoadingPercent,
+                isLoading, setIsLoading,
+                status, setStatus,
+                saveData,
+                error, setError
+            }
+        }>
+            {children}
+        </AppStateContext.Provider>
+    )
 }
 
 export const useAppState = () => {
-	const context = useContext(AppStateContext)
-	if (!context)
-		throw new Error('useAppState must be used within a MyProvider')
-	return context
-}
+    const context = useContext(AppStateContext);
+    if (!context) throw new Error('useAppState must be used within a MyProvider');
+    return context;
+};
