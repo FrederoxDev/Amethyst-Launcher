a11f31e032788207030eb531d97c95e05e6c7dee
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-07-26 00:11:50 +1200

    Shard.ts Completed

--- src/renderer/scripts/Shard.ts
+++ src/renderer/scripts/Shard.ts
@@ -3,239 +3,269 @@ import { SemVersionData } from './SemVersion'
 import AJV_Instance from './AJV_Instance'
 
 // region Shard
-export interface Shard {
-  // UUID v4 string
-  uuid: string
-  version: SemVersionData
-}
-
 export namespace Shard {
-  export const Schema: JSONSchemaType<Shard> = {
-    type: 'object',
-    properties: {
-      uuid: { type: 'string', format: 'uuid' },
-      version: SemVersionData.Schema
-    },
-    required: ['uuid', 'version'],
-    additionalProperties: false
+  // region Shard.Full
+  /**
+   * Contains full data. Used **externally**
+   *
+   * ```ts
+   * meta: {
+   *  name: string
+   *  author: string | string[]
+   *  description?: string
+   *  uuid: string // Must be UUID v4
+   *  version: SemVersionData
+   *  min_launcher_version: SemVersionData
+   *  format?: Shard.Format
+   * }
+   * options?: Shard.Option[]
+   * ```
+   *
+   * @external
+   * @see {Shard.Fragment}
+   * @see {Shard.Format}
+   * @see {Shard.Option}
+   * @see {SemVersionData}
+   */
+  export interface Full {
+    meta: {
+      name: string
+      authors: string | string[]
+      description?: string
+      uuid: string
+      version: SemVersionData
+      min_launcher_version: SemVersionData
+      format?: Shard.Format
+    }
+    options?: Shard.Option[]
   }
+  export namespace Full {
+    export const Schema: JSONSchemaType<Shard.Full> = {
+      type: 'object',
+      properties: {
+        meta: {
+          type: 'object',
+          properties: {
+            name: { type: 'string' },
+            authors: {
+              oneOf: [{ type: 'string' }, { type: 'array', items: { type: 'string' } }]
+            },
+            description: { type: 'string', nullable: true },
+            uuid: { type: 'string', format: 'uuid' },
+            version: SemVersionData.Schema,
+            min_launcher_version: SemVersionData.Schema,
+            format: { nullable: true, oneOf: [Shard.Format.Schema]}
+          },
+          required: ['name', 'authors', 'uuid', 'version', 'min_launcher_version'],
+          additionalProperties: false
+        },
+        options: {
+          type: 'array',
+          items: Option.Schema,
+          nullable: true
+        }
+      },
+      required: ['meta'],
+      additionalProperties: false
+    }
 
-  export const Validator = AJV_Instance.compile<Shard>(Schema)
-}
-// endregion
-
-// region ShardFormat
-export interface ShardFormat {
-  type?: ShardType
-  min_launcher_version: SemVersionData
-}
-
-export enum ShardType {
-  Mod = 0,
-  Runtime = 1
-}
-// endregion
-
-// region ShardData
-export interface ShardData {
-  meta: {
-    name: string
-    authors: string | string[]
-    description?: string
+    export const Validator = AJV_Instance.compile<Shard.Full>(Shard.Full.Schema)
   }
-  format: {
-    type?: ShardType,
+  // endregion
+
+  // region Shard.Fragment
+  /**
+   * Contains minimal data. Used **internally**
+   *
+   * ```ts
+   * uuid: string // Must be UUID v4
+   * version: SemVersionData
+   * ```
+   *
+   * @internal
+   * @see {Shard.Full}
+   * @see {SemVersionData}
+   */
+  export interface Fragment {
+    // UUID v4 string
     uuid: string
     version: SemVersionData
   }
-  options?: ModOption[]
-}
+  export namespace Fragment {
+    export const Schema: JSONSchemaType<Shard.Fragment> = {
+      type: 'object',
+      properties: {
+        uuid: { type: 'string', format: 'uuid' },
+        version: SemVersionData.Schema
+      },
+      required: ['uuid', 'version'],
+      additionalProperties: false
+    }
 
-export namespace ShardData {
-  export const Schema: JSONSchemaType<ShardData> = {
-    type: 'object',
-    properties: {
-      meta: {
-        type: 'object',
-        properties: {
-          name: { type: 'string' },
-          authors: {
-            oneOf: [{ type: 'string' }, { type: 'array', items: { type: 'string' } }]
-          },
-          description: { type: 'string', nullable: true },
-          uuid: { type: 'string', format: 'uuid' },
-          version: SemVersionData.Schema
-        },
-        required: ['name', 'authors', 'uuid', 'version'],
-        additionalProperties: false
-      },
-      format: {
-        type: 'object',
-        properties: {
-          format_version: SemVersionData.Schema,
-          is_runtime: { type: 'boolean', nullable: true }
-        },
-        required: ['format_version'],
-        additionalProperties: false
-      },
-      options: {
-        type: 'array',
-        items: ModOption.Schema,
-        nullable: true
+    export const Validator = AJV_Instance.compile<Shard.Fragment>(Shard.Fragment.Schema)
+  }
+  // endregion
+
+  // region Shard.Format
+  export enum Format {
+    Mod = 0,
+    Runtime = 1
+  }
+
+  export namespace Format {
+    export const Schema: JSONSchemaType<Shard.Format> = {
+      type: 'number',
+      enum: [0, 1]
+    }
+    export const Validator = AJV_Instance.compile<Shard.Format>(Shard.Format.Schema)
+  }
+  // endregion
+
+  // region Shard.Option
+  export type Option = Option.Empty | Option.Text | Option.Toggle | Option.Radial | Option.Slider
+
+  export namespace Option {
+    export interface Empty {
+      type: string
+      properties?: {
+        label?: string
+        description?: string
       }
-    },
-    required: ['meta', 'format'],
-    additionalProperties: false
-  }
-
-  export const Validator = AJV_Instance.compile<ShardData>(Schema)
-}
-// endregion
-
-// region ModOptions
-/** @description Options can only be used in "Mod" Shards (`type: 0`)*/
-export type ModOption = ModOption.Empty | ModOption.Text | ModOption.Toggle | ModOption.Radial | ModOption.Slider
-
-export namespace ModOption {
-  export interface Empty {
-    type: string
-    properties?: {
-      label?: string
-      description?: string
     }
-  }
-  export interface Text extends Empty {
-    type: 'text'
-  }
-  export interface Toggle extends Empty {
-    type: 'toggle'
-  }
-  export interface Radial extends Empty {
-    type: 'radial'
-    properties: {
-      label?: string
-      description?: string
-      options: string[]
+    export interface Text extends Empty {
+      type: 'text'
     }
-  }
-  export interface Slider extends Empty {
-    type: 'slider'
-    properties: {
-      label?: string
-      description?: string
-      min: number
-      max: number
+    export interface Toggle extends Empty {
+      type: 'toggle'
     }
-  }
-
-  export const Schema: JSONSchemaType<ModOption> = {
-    oneOf: [
-      {
-        type: 'object',
-        properties: {
-          type: {
-            type: 'string',
-            pattern: '^empty$'
-          },
-          properties: {
-            type: 'object',
-            properties: {
-              label: { type: 'string', nullable: true },
-              description: { type: 'string', nullable: true }
-            },
-            nullable: true,
-            additionalProperties: false
-          }
-        },
-        required: ['type'],
-        additionalProperties: false
-      },
-      {
-        type: 'object',
-        properties: {
-          type: {
-            type: 'string',
-            pattern: '^text$'
-          },
-          properties: {
-            type: 'object',
-            properties: {
-              label: { type: 'string', nullable: true },
-              description: { type: 'string', nullable: true }
-            },
-            nullable: true,
-            additionalProperties: false
-          }
-        },
-        required: ['type'],
-        additionalProperties: false
-      },
-      {
-        type: 'object',
-        properties: {
-          type: {
-            type: 'string',
-            pattern: '^toggle$'
-          },
-          properties: {
-            type: 'object',
-            properties: {
-              label: { type: 'string', nullable: true },
-              description: { type: 'string', nullable: true }
-            },
-            nullable: true,
-            additionalProperties: false
-          }
-        },
-        required: ['type'],
-        additionalProperties: false
-      },
-      {
-        type: 'object',
-        properties: {
-          type: {
-            type: 'string',
-            pattern: '^radial$'
-          },
-          properties: {
-            type: 'object',
-            properties: {
-              label: { type: 'string', nullable: true },
-              description: { type: 'string', nullable: true },
-              options: { type: 'array', items: { type: 'string' } }
-            },
-            required: ['options'],
-            additionalProperties: false
-          }
-        },
-        required: ['type', 'properties'],
-        additionalProperties: false
-      },
-      {
-        type: 'object',
-        properties: {
-          type: {
-            type: 'string',
-            pattern: '^slider$'
-          },
-          properties: {
-            type: 'object',
-            properties: {
-              label: { type: 'string', nullable: true },
-              description: { type: 'string', nullable: true },
-              min: { type: 'number' },
-              max: { type: 'number' }
-            },
-            required: ['min', 'max'],
-            additionalProperties: false
-          }
-        },
-        required: ['type', 'properties'],
-        additionalProperties: false
+    export interface Radial extends Empty {
+      type: 'radial'
+      properties: {
+        label?: string
+        description?: string
+        options: string[]
       }
-    ]
-  }
+    }
+    export interface Slider extends Empty {
+      type: 'slider'
+      properties: {
+        label?: string
+        description?: string
+        min: number
+        max: number
+      }
+    }
 
-  export const Validator = AJV_Instance.compile<ModOption>(Schema)
+    export const Schema: JSONSchemaType<Option> = {
+      oneOf: [
+        {
+          type: 'object',
+          properties: {
+            type: {
+              type: 'string',
+              const: 'empty'
+            },
+            properties: {
+              type: 'object',
+              properties: {
+                label: { type: 'string', nullable: true },
+                description: { type: 'string', nullable: true }
+              },
+              nullable: true,
+              additionalProperties: false
+            }
+          },
+          required: ['type'],
+          additionalProperties: false
+        },
+        {
+          type: 'object',
+          properties: {
+            type: {
+              type: 'string',
+              const: 'text'
+            },
+            properties: {
+              type: 'object',
+              properties: {
+                label: { type: 'string', nullable: true },
+                description: { type: 'string', nullable: true }
+              },
+              nullable: true,
+              additionalProperties: false
+            }
+          },
+          required: ['type'],
+          additionalProperties: false
+        },
+        {
+          type: 'object',
+          properties: {
+            type: {
+              type: 'string',
+              const: 'toggle'
+            },
+            properties: {
+              type: 'object',
+              properties: {
+                label: { type: 'string', nullable: true },
+                description: { type: 'string', nullable: true }
+              },
+              nullable: true,
+              additionalProperties: false
+            }
+          },
+          required: ['type'],
+          additionalProperties: false
+        },
+        {
+          type: 'object',
+          properties: {
+            type: {
+              type: 'string',
+              const: 'radial'
+            },
+            properties: {
+              type: 'object',
+              properties: {
+                label: { type: 'string', nullable: true },
+                description: { type: 'string', nullable: true },
+                options: { type: 'array', items: { type: 'string' } }
+              },
+              required: ['options'],
+              additionalProperties: false
+            }
+          },
+          required: ['type', 'properties'],
+          additionalProperties: false
+        },
+        {
+          type: 'object',
+          properties: {
+            type: {
+              type: 'string',
+              const: 'slider'
+            },
+            properties: {
+              type: 'object',
+              properties: {
+                label: { type: 'string', nullable: true },
+                description: { type: 'string', nullable: true },
+                min: { type: 'number' },
+                max: { type: 'number' }
+              },
+              required: ['min', 'max'],
+              additionalProperties: false
+            }
+          },
+          required: ['type', 'properties'],
+          additionalProperties: false
+        }
+      ]
+    }
+
+    export const Validator = AJV_Instance.compile<Option>(Schema)
+  }
+  // endregion
 }
 // endregion
