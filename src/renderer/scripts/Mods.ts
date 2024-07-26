8f16fce2515fa5001a861ffe3d6b67d5d06140aa
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-07-26 19:58:25 +1200

    Revert Mods.ts

--- src/renderer/scripts/Mods.ts
+++ src/renderer/scripts/Mods.ts
@@ -1,8 +1,5 @@
 import { ModsFolder } from './Paths'
 
-import Ajv, { JSONSchemaType, DefinedError } from 'ajv'
-const ajv = new Ajv({ allErrors: true, useDefaults: true })
-
 //////////////////// MOD CONFIG ////////////////////
 
 export interface ModConfig {
@@ -15,95 +12,52 @@ export interface ModConfig {
   }
 }
 
-const ModConfigSchema: JSONSchemaType<ModConfig> = {
-  type: 'object',
-  properties: {
-    meta: {
-      type: 'object',
-      properties: {
-        name: { type: 'string', default: '' },
-        version: { type: 'string', default: '' },
-        author: { type: 'string', default: '' },
-        description: { type: 'string', nullable: true },
-        is_runtime: { type: 'boolean', nullable: true }
-      },
-      required: ['name', 'version', 'author']
-    }
-  },
-  required: ['meta'],
-  additionalProperties: false
-}
-
-const Validator = ajv.compile(ModConfigSchema)
-
 export function ValidateMod(config: Record<string, unknown>, outErrors?: string[]): ModConfig {
-  Validator(config)
-
-  if (Validator.errors) {
-    for (const err of Validator.errors as DefinedError[]) {
-      switch (err.keyword) {
-        case 'type': {
-          outErrors?.push(`Field '${err.instancePath}' must be of type '${err.params.type}'`)
-          break
-        }
-      }
-    }
-  }
-
-  return {
+  const validated_config: ModConfig = {
     meta: {
       name: '',
       version: '',
-      author: ''
-    },
-    ...config
+      author: '',
+      description: undefined,
+      is_runtime: undefined
+    }
   }
 
-  // const validated_config: ModConfig = {
-  //   meta: {
-  //     name: '',
-  //     version: '',
-  //     author: '',
-  //     description: undefined,
-  //     is_runtime: undefined
-  //   }
-  // }
-  //
-  // if ('meta' in config && typeof config['meta'] === 'object' && config['meta'] != null) {
-  //   const meta = config['meta']
-  //
-  //   if ('name' in meta) {
-  //     if (typeof meta['name'] !== 'string') {
-  //       outErrors?.push(`field 'name' in 'meta' must be of type 'string'`)
-  //     } else validated_config.meta.name = meta['name']
-  //   } else outErrors?.push(`object 'meta' must contain field 'name' of type 'string'`)
-  //
-  //   if ('version' in meta) {
-  //     if (typeof meta['version'] !== 'string') {
-  //       outErrors?.push(`field 'version' in 'meta' must be of type 'string'`)
-  //     } else validated_config.meta.version = meta['version']
-  //   } else outErrors?.push(`object 'meta' must contain field 'version' of type 'string'`)
-  //
-  //   if ('author' in meta) {
-  //     if (typeof meta['author'] !== 'string') {
-  //       outErrors?.push(`field 'author' in 'meta' must be of type 'string'`)
-  //     } else validated_config.meta.author = meta['author']
-  //   } else outErrors?.push(`object 'meta' must contain field 'author' of type 'string'`)
-  //
-  //   if ('description' in meta) {
-  //     if (typeof meta['description'] !== 'string') {
-  //       outErrors?.push(`field 'description' in 'meta' must be of type 'string'`)
-  //     } else validated_config.meta.description = meta['description']
-  //   }
-  //
-  //   if ('is_runtime' in meta) {
-  //     if (typeof meta['is_runtime'] !== 'boolean') {
-  //       outErrors?.push(`field 'is_runtime' in 'meta' must be of type 'boolean'`)
-  //     } else validated_config.meta.is_runtime = meta['is_runtime']
-  //   }
-  // } else outErrors?.push(`mod.json should have field 'meta' of type 'object'`)
-  //
-  // return validated_config
+  if ('meta' in config && typeof config['meta'] === 'object' && config['meta'] != null) {
+    const meta = config['meta']
+
+    if ('name' in meta) {
+      if (typeof meta['name'] !== 'string') {
+        outErrors?.push(`field 'name' in 'meta' must be of type 'string'`)
+      } else validated_config.meta.name = meta['name']
+    } else outErrors?.push(`object 'meta' must contain field 'name' of type 'string'`)
+
+    if ('version' in meta) {
+      if (typeof meta['version'] !== 'string') {
+        outErrors?.push(`field 'version' in 'meta' must be of type 'string'`)
+      } else validated_config.meta.version = meta['version']
+    } else outErrors?.push(`object 'meta' must contain field 'version' of type 'string'`)
+
+    if ('author' in meta) {
+      if (typeof meta['author'] !== 'string') {
+        outErrors?.push(`field 'author' in 'meta' must be of type 'string'`)
+      } else validated_config.meta.author = meta['author']
+    } else outErrors?.push(`object 'meta' must contain field 'author' of type 'string'`)
+
+    if ('description' in meta) {
+      if (typeof meta['description'] !== 'string') {
+        outErrors?.push(`field 'description' in 'meta' must be of type 'string'`)
+      } else validated_config.meta.description = meta['description']
+    }
+
+    if ('is_runtime' in meta) {
+      if (typeof meta['is_runtime'] !== 'boolean') {
+        outErrors?.push(`field 'is_runtime' in 'meta' must be of type 'boolean'`)
+      } else validated_config.meta.is_runtime = meta['is_runtime']
+    }
+  } else outErrors?.push(`mod.json should have field 'meta' of type 'object'`)
+
+  return validated_config
 }
 
 //////////////////// MOD LIST ////////////////////
