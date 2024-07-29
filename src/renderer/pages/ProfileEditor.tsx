f7056ad94f4379cc73b3b2d0c6a04ce8a95bdfae
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-07-30 00:35:56 +1200

    ProfileEditor.tsx Optimisations and Changes

--- src/renderer/pages/ProfileEditor.tsx
+++ src/renderer/pages/ProfileEditor.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useMemo, useState } from 'react'
 import Panel from '../components/Panel'
 import TextInput from '../components/TextInput'
 import Dropdown from '../components/Dropdown'
@@ -6,111 +6,106 @@ import MinecraftButton from '../components/MinecraftButton'
 import { MinecraftButtonStyle } from '../components/MinecraftButtonStyle'
 import { UseAppState } from '../contexts/AppState'
 import { useNavigate } from 'react-router-dom'
-import { GetLatestVersion, Version } from '../scripts/types/Version'
+import { /** GetDefaultVersionPath, */ GetLatestVersion, Version } from '../scripts/types/Version'
 import ListItem from '../components/ListItem'
 import List from '../components/List'
 import Shard, { GetShards } from '../scripts/types/Shard'
-import { GetProfiles } from '../scripts/types/Profile'
-// import { GetDefaultInstallPath } from '../scripts/VersionManager'
 
 export default function ProfileEditor() {
-  const [profileName, setProfileName] = useState('')
-  const [profileActiveMods, setProfileActiveMods] = useState<Shard.Reference[] | undefined>([])
-  const [profileRuntime, setProfileRuntime] = useState<Shard.Reference | undefined>(undefined)
-  const [profileMinecraftVersion, setProfileMinecraftVersion] = useState<Version>(GetLatestVersion())
-  // const [profileInstallDir, setProfileInstallDir] = useState<string>(GetDefaultInstallPath())
+  const [profile_name, SetProfileName] = useState('')
+  const [profile_mods, SetProfileMods] = useState<Shard.Reference[] | undefined>(undefined)
+  const [profile_runtime, SetProfileRuntime] = useState<Shard.Reference | undefined>(undefined)
+  const [profile_version, SetProfileVersion] = useState<Version>(GetLatestVersion)
+  // const [profile_path, SetProfilePath] = useState<string>(GetDefaultVersionPath)
 
-  const { mods, runtimes, versions, profiles, SetProfiles, selected_profile, saveData, SetMods } = UseAppState()
+  const [sub_page, SetSubPage] = useState<string>('Mods')
+
+  const { mods, runtimes, versions, profiles, SetProfiles, selected_profile, SaveState, SetMods } = UseAppState()
   const navigate = useNavigate()
 
-  if (profiles.length === 0) navigate('/profile-manager')
+  const profile = useMemo(() => {
+    return profiles[selected_profile]
+  }, [profiles, selected_profile])
 
-  const toggleModActive = (mod: Shard.Manifest) => {
-    if (profileActiveMods) {
+  useEffect(() => {
+    if (profile) {
+      SetProfileName(profile.name)
+      SetProfileRuntime(profile.runtime)
+      SetProfileMods(profile.mods)
+      SetProfileVersion(profile.version)
+    }
 
-      const active_mod_uuids = profileActiveMods.map(m => m.uuid)
+    if (profile === undefined) {
+      navigate('/profile-manager')
+    }
+    if (profile_runtime === undefined) {
+      SetSubPage('Settings')
+    }
+  }, [navigate, profile, profile_runtime])
+
+
+  const ToggleMod = useCallback((mod: Shard.Manifest) => {
+    if (profile_mods) {
+
+      const active_mod_uuids = profile_mods.map(m => m.uuid)
 
       if (active_mod_uuids.includes(mod.meta.uuid)) {
-        const newActive = profileActiveMods.filter(m => m.uuid !== mod.meta.uuid)
-        setProfileActiveMods(newActive)
+        const newActive = profile_mods.filter(m => m.uuid !== mod.meta.uuid)
+        SetProfileMods(newActive)
       } else {
-        const newActive = [...profileActiveMods, Shard.Manifest.toFragment(mod)]
-        setProfileActiveMods(newActive)
+        const newActive = [...profile_mods, Shard.Manifest.toFragment(mod)]
+        SetProfileMods(newActive)
       }
     }
     // no active mods, so this mod must be toggling to active. just add it to the active mods
     else {
       const newActive = [Shard.Manifest.toFragment(mod)]
       console.log(newActive)
-      setProfileActiveMods(newActive)
+      SetProfileMods(newActive)
     }
-  }
+  }, [profile_mods])
 
-  const ModButton = ({ mod }: { mod: Shard.Manifest }) => {
+  const ModButton = useCallback(({ mod }: { mod: Shard.Manifest }) => {
     return (
-      <ListItem
-        className="cursor-pointer"
-        onClick={() => {
-          if (profileRuntime === undefined) {
-            alert('Cannot add mods to a vanilla profile')
-            return
-          }
-
-          toggleModActive(mod)
-        }}
-      >
+      <ListItem className="cursor-pointer" onClick={() => ToggleMod(mod)}>
         <div className="p-[4px]">
           <p className="minecraft-seven text-white">{mod.meta.name}</p>
         </div>
       </ListItem>
     )
-  }
+  }, [ToggleMod])
 
-  const loadProfile = useCallback(() => {
-    const profile = GetProfiles()[selected_profile]
-
-    if (profile) {
-      setProfileName(profile.name)
-      setProfileRuntime(profile.runtime)
-      setProfileActiveMods(profile.mods)
-      setProfileMinecraftVersion(profile.version)
-    }
-
-  }, [selected_profile])
-
-  const saveProfile = () => {
-
-
-    if (profileRuntime) {
+  const SaveProfile = () => {
+    if (profile_runtime) {
 
       const runtime_uuids = runtimes.map(r => r.meta.uuid)
 
-      if (!runtime_uuids.includes(profileRuntime.uuid)) {
-        setProfileRuntime(undefined)
+      if (!runtime_uuids.includes(profile_runtime.uuid)) {
+        SetProfileRuntime(undefined)
       }
     }
 
-    if (profileActiveMods) {
+    if (profile_mods) {
       const mod_uuids = mods.map(m => m.meta.uuid)
 
-      const newMods = profileActiveMods.filter(mod => mod_uuids.includes(mod.uuid))
-      setProfileActiveMods(newMods)
+      const newMods = profile_mods.filter(mod => mod_uuids.includes(mod.uuid))
+      SetProfileMods(newMods)
     }
 
-    profiles[selected_profile].name = profileName
-    profiles[selected_profile].runtime = profileRuntime
-    profiles[selected_profile].mods = profileActiveMods
-    profiles[selected_profile].version = profileMinecraftVersion
+    profiles[selected_profile].name = profile_name
+    profiles[selected_profile].runtime = profile_runtime
+    profiles[selected_profile].mods = profile_mods
+    profiles[selected_profile].version = profile_version
 
-    saveData()
+    SaveState()
     navigate('/profile-manager')
   }
 
-  const deleteProfile = () => {
+  const DeleteProfile = () => {
     profiles.splice(selected_profile, 1)
     SetProfiles(profiles)
 
-    saveData()
+    SaveState()
     navigate('/profile-manager')
   }
 
@@ -120,130 +115,158 @@ export default function ProfileEditor() {
     SetMods(shards.filter(s => s.meta.format === 0 || s.meta.format === undefined))
   }, [SetMods])
 
-  useEffect(() => {
-    loadProfile()
-  }, [loadProfile])
+  const [version_uuids, version_names, version_options] = useMemo(() => {
+    const version_options = versions.filter(ver => ver.format === Version.Format.Release)
+    const version_uuids = version_options.map(v => v.uuid)
+    const version_names = version_options.map(v => Version.toString(v))
 
-  const release_versions = versions.filter(ver => ver.format === Version.Format.Release)
-  const version_uuids = release_versions.map(v => v.uuid)
-  const version_names = release_versions.map(v => Version.toString(v))
+    return [version_uuids, version_names, version_options]
+  }, [versions])
 
-  const runtime_options = [undefined, ...runtimes]
-  const runtime_names = runtime_options.map(r => {
-    if (r) {
-      return r.meta.name
-    }
-    else return 'Vanilla'
-  })
-  const runtime_uuids = runtime_options.map(r => {
-    if (r) {
-      return r.meta.uuid
-    }
-    else return ''
-  })
+  const [runtime_names, runtime_options, runtime_index] = useMemo(() =>{
+    const runtime_options = [undefined, ...runtimes]
 
-  let runtime_index = 0
-  if (profileRuntime) {
-    runtime_index = runtime_uuids.indexOf(profileRuntime.uuid)
-  }
-
-  let active_mods: Shard.Manifest[] = []
-  let inactive_mods: Shard.Manifest[] = mods
-
-  if (profileActiveMods) {
-    const active_mod_uuids = profileActiveMods.map(m => m.uuid)
-
-    active_mods = mods.filter(mod => {
-      return active_mod_uuids.includes(mod.meta.uuid)
+    const runtime_names = runtime_options.map(r => {
+      if (r) {
+        return r.meta.name
+      } else return 'Vanilla'
     })
 
-    inactive_mods = mods.filter(mod => {
-      return !active_mod_uuids.includes(mod.meta.uuid)
+    const runtime_uuids =  runtime_options.map(r => {
+      if (r) {
+        return r.meta.uuid
+      }
+      else return ''
     })
-  }
+
+    const runtime_index = profile_runtime !== undefined ? runtime_uuids.indexOf(profile_runtime.uuid) : 0
+
+    return [runtime_names, runtime_options, runtime_index]
+  }, [profile_runtime, runtimes])
+
+  const [active_mods, inactive_mods]: [Shard.Manifest[], Shard.Manifest[]] = useMemo(() => {
+    if (profile_mods) {
+      const active_mod_uuids = profile_mods.map(m => m.uuid)
+
+      const active_mods = mods.filter(mod => {
+        return active_mod_uuids.includes(mod.meta.uuid)
+      })
+
+      const inactive_mods = mods.filter(mod => {
+        return !active_mod_uuids.includes(mod.meta.uuid)
+      })
+
+      return [active_mods, inactive_mods]
+    }
+    else {
+      return [[], mods]
+    }
+  }, [mods, profile_mods])
   
   return (
     <Panel>
       <div className="flex flex-col gap-[8px] w-full h-full">
-        <div className="w-full shrink-0">
-          <div className="content_panel">
-            <div className="flex flex-row gap-[8px]">
-              <div className="w-[40px] h-[40px] border-[3px] border-[#1E1E1F] box-content">
-                <img src={profiles[selected_profile]?.icon_path ?? `/images/icons/earth-icon.png`} className="w-full h-full pixelated" alt="" />
+        <div className="flex flex-row gap-[8px]">
+          <div className="w-[48px] h-[48px] border-[3px] border-[#1E1E1F] box-content">
+            <img src={profile?.icon_path ?? `/images/icons/earth-icon.png`}
+                 className="w-full h-full pixelated" alt="" />
+          </div>
+          <div className="flex flex-row gap-[8px] justify-between flex-grow h-[48px] border-[3px] border-[#1E1E1F] bg-[#48494a] box-content overflow-hidden">
+              <div className="flex flex-row gap-[8px] p-[8px] justify-between">
+                <div className="flex flex-col gap-[2px]">
+                  <p className="minecraft-seven text-white text-[18px]">{profile_name}</p>
+                  <p className="minecraft-seven text-[#B1B2B5] text-[12px] mt-auto">{`${profile_runtime?.name ?? 'Vanilla'} ${profile_version.sem_version}`}</p>
+                </div>
               </div>
-              <div className="flex flex-col gap-[2px] justify-center">
-                <p className="minecraft-seven text-white text-[16px]">{profiles[selected_profile]?.name}</p>
-                <p className="minecraft-seven text-[#B1B2B5] text-[14px]">{`${profiles[selected_profile]?.runtime?.name ?? 'Vanilla'}  ${ profiles[selected_profile]?.version.sem_version }`}</p>
+            <div className="flex flex-row gap-[4px] p-[4px] ">
+              {
+                profile_runtime !== undefined && (
+                  <div className="flex w-fit border-[3px] border-[#1E1E1F] cursor-pointer"
+                       onClick={() => SetSubPage('Mods')}>
+                    <div className="flex justify-center p-[4px] list_item_border">
+                      <p className="minecraft-seven text-white text-[14px]">{'Mods'}</p>
+                    </div>
+                  </div>
+                )
+              }
+              <div className="flex w-fit border-[3px] border-[#1E1E1F] cursor-pointer"
+                   onClick={() => SetSubPage('Settings')}>
+                <div className="flex justify-center p-[4px] list_item_border">
+                  <p className="minecraft-seven text-white text-[14px]">{'Settings'}</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
         <div className="content_panel">
-          {/* Settings */}
-          <div className="flex flex-col gap-[8px]">
-            <TextInput label="Profile Name" text={profileName} setText={setProfileName} />
-            <Dropdown
-              labelText="Minecraft Version"
-              default_index={version_uuids.indexOf(profileMinecraftVersion.uuid)}
-              SetIndex={
-                (index) => {
-                  setProfileMinecraftVersion(release_versions[index])
-                }
-              }
-              // we don't support non-release versions right now so only show release lmao
-              options={version_names}
-              id="minecraft-version"
-            />
-            <Dropdown
-              labelText="Runtime"
-              default_index={runtime_index}
-              SetIndex={(index) => {
-                if (runtime_options[index]) {
-                  setProfileRuntime(Shard.Manifest.toFragment(runtime_options[index]))
-                }
-                else setProfileRuntime(undefined)
-              }}
-              options={runtime_names}
-              id="runtime-mod"
-            />
-            {/*<TextInput label="Install Directory" text={profileInstallDir} setText={setProfileInstallDir} />*/}
-          </div>
+          { sub_page === 'Settings' && (
+            <>
+              {/* Settings */}
+              <div className="flex flex-col gap-[8px]">
+                <TextInput label="Profile Name" text={profile_name} setText={SetProfileName} />
+                <Dropdown
+                  labelText="Minecraft Version"
+                  default_index={version_uuids.indexOf(profile_version.uuid)}
+                  SetIndex={
+                    (index) => {
+                      SetProfileVersion(version_options[index])
+                    }
+                  }
+                  // we don't support non-release versions right now so only show release lmao
+                  options={version_names}
+                  id="minecraft-version"
+                />
+                <Dropdown
+                  labelText="Runtime"
+                  default_index={runtime_index}
+                  SetIndex={(index) => {
+                    if (runtime_options[index]) {
+                      SetProfileRuntime(Shard.Manifest.toFragment(runtime_options[index]))
+                    } else SetProfileRuntime(undefined)
+                  }}
+                  options={runtime_names}
+                  id="runtime-mod"
+                />
+                {/*<TextInput label="Install Directory" text={profileInstallDir} setText={setProfileInstallDir} />*/}
+              </div>
 
-          {/* Mod Selection */}
-          {profileRuntime === undefined ? (
-            <div className="flex-grow flex justify-around">
-              <div className="h-full flex flex-col"></div>
-            </div>
-          ) : (
-            <div className="bg-[#48494a] flex-grow flex justify-around gap-[8px]">
-              <div className="w-[50%] h-full flex flex-col">
-                <p className="text-white minecraft-seven text-[14px]">Active Mods</p>
-                <List>
-                  {
-                    active_mods.map((mod, index) => {
-                      return <ModButton mod={mod} key={index} />
-                    })
-                  }
-                </List>
+              {/* Mod Selection */}
+              {profile_runtime === undefined ? (
+                <div className="flex-grow flex justify-around">
+                  <div className="h-full flex flex-col"></div>
+                </div>
+              ) : (
+                <div className="bg-[#48494a] flex-grow flex justify-around gap-[8px]">
+                  <div className="w-[50%] h-full flex flex-col">
+                    <p className="text-white minecraft-seven text-[14px]">Active Mods</p>
+                    <List>
+                      {
+                        active_mods.map((mod, index) => {
+                          return <ModButton mod={mod} key={index} />
+                        })
+                      }
+                    </List>
+                  </div>
+                  <div className=" w-[50%] h-full flex flex-col">
+                    <p className="text-white minecraft-seven text-[14px]">Inactive Mods</p>
+                    <List>
+                      {
+                        inactive_mods.map((mod, index) => {
+                          return <ModButton mod={mod} key={index} />
+                        })
+                      }
+                    </List>
+                  </div>
+                </div>
+              )}
+
+              {/* Profile Actions */}
+              <div className="flex justify-around gap-[8px]">
+                <MinecraftButton text="Save Profile" onClick={() => SaveProfile()} />
+                <MinecraftButton text="Delete Profile" style={MinecraftButtonStyle.Warn} onClick={() => DeleteProfile()} />
               </div>
-              <div className=" w-[50%] h-full flex flex-col">
-                <p className="text-white minecraft-seven text-[14px]">Inactive Mods</p>
-                <List>
-                  {
-                    inactive_mods.map((mod, index) => {
-                      return <ModButton mod={mod} key={index} />
-                    })
-                  }
-                </List>
-              </div>
-            </div>
+            </>
           )}
-
-          {/* Profile Actions */}
-          <div className="flex justify-around gap-[8px]">
-            <MinecraftButton text="Save Profile" onClick={() => saveProfile()} />
-            <MinecraftButton text="Delete Profile" style={MinecraftButtonStyle.Warn} onClick={() => deleteProfile()} />
-          </div>
         </div>
       </div>
     </Panel>
