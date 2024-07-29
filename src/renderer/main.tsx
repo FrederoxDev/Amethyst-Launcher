71a3f5ce02b21eac96c850f09b99a8b3060c568e
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-07-29 23:22:53 +1200

    Enable React StrictMode

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 
@@ -6,7 +6,9 @@ import App from './App.tsx'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
+  <StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </StrictMode>
 )
