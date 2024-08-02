ff6e3aaed9bb807c495576ca4ac64759ae46ae8a
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-08-03 03:02:21 +1200

    VersionManager.tsx Changes

--- src/renderer/pages/VersionManager.tsx
+++ src/renderer/pages/VersionManager.tsx
@@ -1,22 +1,11 @@
 import { GetVersions, RefreshVersionsFile, Version } from '../scripts/types/Version'
 import { useState } from 'react'
-import { ipcRenderer } from 'electron'
-import PopupPanel from '../components/PopupPanel'
+import { clipboard, ipcRenderer } from 'electron'
 import { Console } from '../scripts/types/Console'
 
 import * as fs from 'fs'
-import * as child from 'child_process'
-import Panel from '../components/Panel'
-import ListItem from '../components/ListItem'
-import List from '../components/List'
 
-type VersionButtonProps = {
-  version: Version
-  onInspect: () => void
-  onDelete: () => void
-}
-
-const VersionButton = ({ version, onInspect, onDelete }: VersionButtonProps) => {
+const VersionButton = (version: Version, index: number, selected_index: number | undefined, SetSelectedIndex: (index: number | undefined) => void, OnDelete: () => void) => {
   function DeleteVersion() {
     const message_args = {
       message: 'Are you sure you want to delete this version?\n\nThis is an irreversible action!',
@@ -36,7 +25,7 @@ const VersionButton = ({ version, onInspect, onDelete }: VersionButtonProps) => 
               if (err) {
                 Console.Error((err as Error).message)
               } else {
-                onDelete()
+                OnDelete()
                 Console.Group(Console.ActionStr(`Delete Version`), () => {
                   Console.Info(`Version: ${version.sem_version}`)
                   Console.Group(Console.InfoStr('Path'), () => {
@@ -51,49 +40,65 @@ const VersionButton = ({ version, onInspect, onDelete }: VersionButtonProps) => 
     })
   }
 
-  function OpenVersionLocation() {
-    if (version.path) {
-      if (fs.existsSync(version.path)) {
-        child.spawn(`explorer "${version.path}"`, { shell: true })
-      }
-    }
-  }
+  let version_format = 'Release'
 
-  function InspectVersion() {
-    onInspect()
-  }
+  if (version.format === Version.Format.Beta) version_format = 'Beta'
+  if (version.format === Version.Format.Preview) version_format = 'Preview'
 
   return (
-    <ListItem>
-      <div className="flex flex-row w-full justify-between items-center">
-        <div className="h-full flex flex-col justify-center items-center min-w-0 p-[8px]">
-          <p className="minecraft-seven text-[#FFFFFF] text-[16px]">{version.sem_version}</p>
-          {/*<p className="minecraft-seven text-[#B1B2B5] text-[14px] overflow-ellipsis overflow-hidden whitespace-nowrap">{"Path:"} ({version.path})</p>*/}
+    <div key={index}>
+      <div className="list_item flex flex-row">
+        <div className="flex flex-grow inset_button cursor-pointer" onClick={() => { SetSelectedIndex(selected_index === index ? undefined : index)}}>
+          <div className="flex flex-row w-full justify-between items-center p-[8px]">
+            <div className="flex flex-row gap-[8px]">
+              <p className="minecraft-seven text-white text-[14px]">{version.sem_version}</p>
+              <p className="minecraft-seven text-[#B1B2B5] text-[14px]">{version_format}</p>
+            </div>
+            <div className="w-[30px] h-[30px] p-[10px]">
+              <img src={selected_index === index ? `/images/icons/chevron-up.png` : `/images/icons/chevron-down.png`}
+                   className="w-full h-full pixelated" alt="" />
+            </div>
+          </div>
         </div>
-        <div className="shrink-0 flex flex-row p-[8px] gap-[8px] justify-right items-center">
-          <div
-            className="shrink-0 cursor-pointer w-[24px] h-[24px] bg-[#333334] hover:bg-[#FF000080] box-content border-[3px] border-[#1E1E1F] rounded-[3px]"
-            onClick={() => DeleteVersion()}
-          >
-            <img src="images/icons/delete-icon.png" className="w-full h-full pixelated" alt="" />
-          </div>
-
-          <div
-            className="w-[24px] h-[24px] shrink-0 bg-[#313233] box-content border-[3px] border-[#1E1E1F] rounded-[3px] cursor-pointer hover:border-[#5a5b5c] hover:bg-[#48494A] active:border-[#4f913c] active:bg-[#3c8527]"
-            onClick={() => OpenVersionLocation()}
-          >
-            <img src="images/icons/open-folder-icon.png" className="w-full h-full pixelated" alt="" />
-          </div>
-
-          <div
-            className="w-[24px] h-[24px] shrink-0 bg-[#313233] box-content border-[3px] border-[#1E1E1F] rounded-[3px] cursor-pointer hover:border-[#5a5b5c] hover:bg-[#48494A] active:border-[#4f913c] active:bg-[#3c8527]"
-            onClick={() => InspectVersion()}
-          >
-            <img src="images/icons/info-icon.png" className="w-full h-full pixelated" alt="" />
-          </div>
+        <div className="w-[58px] h-[58px] p-[8px] flex justify-center items-center inset_button cursor-pointer" onClick={() => DeleteVersion()}>
+          <img src="/images/icons/delete-icon.png" className="pixelated" alt="" />
         </div>
       </div>
-    </ListItem>
+      <div
+        className={`flex flex-col p-[8px] bg-[#313233] border-[3px] m-[-3px] border-[#1e1e1f] overflow-hidden ${selected_index === index ? '' : 'hidden'}`}
+      >
+        <p
+          className="minecraft-seven text-[#B1B2B5] text-[14px] leading-tight min-w-0 overflow-ellipsis overflow-hidden whitespace-nowrap">
+          {`UUID: ${version.uuid}`}
+        </p>
+        <p
+          className="minecraft-seven text-[#B1B2B5] text-[14px] leading-tight min-w-0 overflow-ellipsis overflow-hidden whitespace-nowrap">
+          {'Version: ' + version.sem_version}
+        </p>
+        <p
+          className="minecraft-seven text-[#B1B2B5] text-[14px] leading-tight min-w-0 overflow-ellipsis overflow-hidden whitespace-nowrap">
+          {'Format: ' + version.format}
+        </p>
+        {
+          version.path !== undefined && (
+            <div className="flex flex-row justify-between">
+              <p
+                className="minecraft-seven text-[#B1B2B5] text-[14px] leading-tight min-w-0 overflow-ellipsis overflow-hidden whitespace-nowrap">
+                {'Path: ' + version.path}
+              </p>
+              <div
+                className="w-[24px] h-[24px] shrink-0 bg-[#313233] box-content border-[3px] border-[#1E1E1F] rounded-[3px] cursor-pointer hover:border-[#48494A] hover:bg-[#5a5b5c] active:border-[#4f913c] active:bg-[#3c8527]"
+                onClick={() => {
+                  if (version.path) clipboard.writeText(version.path)
+                }}
+              >
+                <img src="/images/icons/copy-icon.png" className="w-full h-full pixelated" alt="" />
+              </div>
+            </div>
+          )
+        }
+      </div>
+    </div>
   )
 }
 
@@ -106,54 +111,32 @@ export default function VersionManager() {
     SetVersions(GetVersions())
   }
 
-  const [selected_version, SetSelectedVersion] = useState<Version | undefined>(undefined)
+  const [selected_version, SetSelectedVersion] = useState<number | undefined>(undefined)
 
   return (
     <>
-      <Panel>
-        <div className="content_panel">
-          <p className="minecraft-seven text-[#FFFFFF] text-[14px]">Version Manager</p>
-          <List>
-            {versions.map((version, index) => {
-              return (
-                <VersionButton
-                  version={version}
-                  onInspect={() => SetSelectedVersion(version)}
-                  onDelete={() => RefreshVersions()}
-                  key={index}
-                />
-              )
-            })}
-          </List>
-        </div>
-      </Panel>
-
-      {selected_version && (
-        <PopupPanel>
-          <div className="flex flex-row w-full justify-between items-center border-y-[3px] border-t-[#5a5b5c] border-b-[#333334] bg-[#48494a] p-[8px]">
-            <p className="minecraft-seven text-white text-[14px] max-w-[400px]">{selected_version.sem_version}</p>
-
-            <div
-              className="p-[4px] justify-center items-center ml-auto cursor-pointer"
-              onClick={() => SetSelectedVersion(undefined)}
-            >
-              <svg width="12" height="12" viewBox="0 0 12 12">
-                <polygon
-                  className="fill-[#FFFFFF]"
-                  fillRule="evenodd"
-                  points="11 1.576 6.583 6 11 10.424 10.424 11 6 6.583 1.576 11 1 10.424 5.417 6 1 1.576 1.576 1 6 5.417 10.424 1"
-                />
-              </svg>
+      <div className="content_panel h-fit max-h-full overflow-y-auto overflow-x-hidden scrollbar">
+        <div className="flex flex-col gap-[24px]">
+          <div className="flex flex-col w-full">
+            <div className="flex flex-row w-full align-bottom">
+              <div
+                className="border-[3px] bg-[#48494a] border-[#1E1E1F] border-b-[0px] px-[8px] py-[4px] w-fit mr-[-3px]">
+                <p className="minecraft-seven text-white text-[14px]">Versions</p>
+              </div>
+              <div className="flex flex-col grow-[1] h-fit mt-auto">
+                <div className="mt-auto bg-[#1E1E1F] h-[3px] " />
+                <div
+                  className="mt-auto border-x-[3px] box-content border-r-[#1E1E1F] bg-[#48494a] border-l-[#48494a] h-[7px] grow-[1]" />
+              </div>
+            </div>
+            <div className="flex flex-col w-full gap-[3px] border-[3px] border-[#1E1E1F] bg-[#313233]">
+              {versions.map((version, index) => {
+                return VersionButton(version, index, selected_version, SetSelectedVersion, RefreshVersions)
+              })}
             </div>
           </div>
-          <div className="flex flex-col w-full border-y-[3px] border-t-[#5a5b5c] border-b-[#333334] bg-[#48494a] p-[8px]">
-            <p className="minecraft-seven text-[#BCBEC0] text-[12px] select-text">{selected_version.path}</p>
-          </div>
-          <div className="flex flex-col w-full border-y-[3px] border-t-[#5a5b5c] border-b-[#333334] bg-[#48494a] p-[8px]">
-            <p className="minecraft-seven text-[#BCBEC0] text-[12px] select-text">{selected_version.uuid}</p>
-          </div>
-        </PopupPanel>
-      )}
+        </div>
+      </div>
     </>
   )
 }
