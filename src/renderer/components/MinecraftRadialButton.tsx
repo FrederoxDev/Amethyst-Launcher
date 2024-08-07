5e9a3ee1b43d94fdd01514a57d08b1b0b951d652
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-08-07 22:05:00 +1200

    MinecraftRadialButton.tsx Changes

--- src/renderer/components/MinecraftRadialButton.tsx
+++ src/renderer/components/MinecraftRadialButton.tsx
@@ -3,17 +3,25 @@ type RadialButtonProperties = {
   value: string
   selected: boolean
   className?: string
+  dark?: boolean
   onChange: (value: string) => void
 }
 
-export default function MinecraftRadialButton({ text, value, selected, className, onChange }: RadialButtonProperties) {
+export default function MinecraftRadialButton({
+                                                text,
+                                                value,
+                                                selected,
+                                                className,
+                                                dark,
+                                                onChange
+                                              }: RadialButtonProperties) {
   return (
     <div className={`radial_button_base ${className}`} onClick={() => onChange(value)}>
-      <div className={`radial_button_border ${selected ? 'selected' : ''}`}>
-        <div className={`radial_button_top ${selected ? 'selected' : ''}`}>
-          <p className={`radial_button_text ${selected ? 'selected' : ''}`}>{text}</p>
+      <div className={`radial_button_border ${selected ? 'selected' : ''} ${dark ? 'dark' : ''}`}>
+        <div className={`radial_button_top ${selected ? 'selected' : ''} ${dark ? 'dark' : ''}`}>
+          <p className={`radial_button_text ${selected ? 'selected' : ''} ${dark ? 'dark' : ''}`}>{text}</p>
         </div>
-        <div className={`radial_button_side ${selected ? 'selected' : ''}`} />
+        <div className={`radial_button_side ${selected ? 'selected' : ''} ${dark ? 'dark' : ''}`} />
       </div>
     </div>
   )
